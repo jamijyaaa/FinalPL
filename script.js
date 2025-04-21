949d1fe95ad28ccf1b91a08c4a0a5992eb483f75
jamijyaaa <eshmurzaevajamilya@gmail.com>
2025-04-22 01:05:43 +0600

    ceated footer section

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const navLinks = document.querySelectorAll(".nav-menu .nav-link");
 const menuOpenButton = document.querySelector("#menu-open-button");
 const menuCloseButton = document.querySelector("#menu-close-button");
 
@@ -6,6 +7,11 @@ document.body.classList.toggle("show-mobile-menu");
 } );
 
 menuCloseButton.addEventListener("click", () => menuOpenButton.click ());
+navLinks.forEach(link => {
+  link.addEventListener("click", () => menuOpenButton.click());
+}
+
+);
 
 //Initialize Swiper
 const swiper = new Swiper('.slider-wrapper', {
